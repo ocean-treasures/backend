7b8e1d61d952955e4b6ada27d6ce451e84cea99f
NikoletaDalaktchieva <nikoletadal@gmail.com>
2017-07-05 11:41:00 +0300

    Admin panel

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pictures
+from .models import Progress
+
+admin.site.register(Pictures)
+admin.site.register(Progress)
--- game/models.py
+++ game/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 class Pictures(models.Model):
 	pictureUrl = models.CharField(max_length = 250)
-	id = models.IntegerField(primary_key = True)
+	pic_id = models.IntegerField(primary_key = True)
 	word = models.CharField(max_length = 30)
 
 class Progress(models.Model):
 	curr = models.IntegerField()
-	max  = models.IntegerField() 
-
-
+	max_progress  = models.IntegerField() 
