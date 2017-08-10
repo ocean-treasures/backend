1f92d86e3629731586d1c058306a59f65fcc5ea4
Stela <stela.d.dineva@gmail.com>
2017-08-10 22:44:33 +0300

    Add STATIC_ROOT and MEDIA_ROOT

--- backend/settings.py
+++ backend/settings.py
@@ -114,3 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/oceantreasures/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/oceantreasures/media/'
